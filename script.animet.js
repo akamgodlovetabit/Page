f941f8fd6f9508df0dd6747aca1c0233a6ffeac8
akamgodlovetabit <164500332+akamgodlovetabit@users.noreply.github.com>
2024-03-24 03:33:31 +0100

    Add files via upload
    first commit

---
+++ script.animet.js
@@ -0,0 +1,21 @@
+/** @format */
+
+const body = document.querySelector("body");
+sidebar = body.querySelector(".side-bar");
+toggle = body.querySelector(".toggle");
+searchBtn = body.querySelector(".search-box");
+modeSwitch = body.querySelector(".toggle-switch");
+modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+	sidebar.classList.toggle("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+	body.classList.toggle("dark");
+	if (body.classList.contains("dark")) {
+		modeText.innerText = "Dark Mode";
+	} else {
+		modeText.innerText = "Light Mode";
+	}
+});
